209c1333b4cc4886165bb4d6e399955a2ed0a9cb
Álvaro Muñoz Vaquero <alvaromunozvaquero@gmail.com>
2023-11-02 03:16:49 +0100

    ...

---
+++ src/Author.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Author {
+    private String name;
+    private String surname;
+    private String date;
+    private String location;
+    private ArrayList<Book> books;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String dateOfPublication;
+    private ArrayList<Author> authors;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDateOfPublication() {
+        return dateOfPublication;
+    }
+
+    public void setDateOfPublication(String dateOfPublication) {
+        this.dateOfPublication = dateOfPublication;
+    }
+
+    public ArrayList<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(ArrayList<Author> authors) {
+        this.authors = authors;
+    }
+}
